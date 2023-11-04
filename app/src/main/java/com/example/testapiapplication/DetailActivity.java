c04c1d4244688409b4f4ce896818235babe5e48e
Azura <azurawithluz@gmail.com>
2023-11-04 13:23:33 +0700

    News App rss

--- app/src/main/java/com/example/testapiapplication/DetailActivity.java
+++ app/src/main/java/com/example/testapiapplication/DetailActivity.java
@@ -2,8 +2,13 @@ package com.example.testapiapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class DetailActivity extends AppCompatActivity {
     @SuppressLint("SetJavaScriptEnabled")
@@ -11,11 +16,32 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
         WebView webView = findViewById(R.id.webView);
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+
         String link = getIntent().getStringExtra("link");
-        webView.getSettings().setJavaScriptEnabled(true);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                progressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+
         assert link != null;
         webView.loadUrl(link);
+
 
 
     }
